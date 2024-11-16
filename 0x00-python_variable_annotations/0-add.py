0702547b8b248b568291169fc2973ecb4f871b5e
youssef3332-a11y <youssef17102003@gmail.com>
2024-11-16 23:09:49 +0100

    implement basic variable annotation functions and add README

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""takes a float a and a float b as arguments and returns their sum"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats"""
+    return a + b
---
+++ 0x00-python_variable_annotations/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+add = __import__('0-add').add
+print(add(1, 2))
