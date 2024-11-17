a41545bb3d61302d8be1a660f7683845ea584dcf
youssef3332-a11y <youssef17102003@gmail.com>
2024-11-17 02:04:11 +0100

    implement asynchronous function to wait for a random delay and add test script

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""takes a float a and a float b as arguments and returns their sum"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """wait for max_delay seconds"""
+    wait = random.uniform(0, max_delay)
+    await asyncio.sleep(wait)
+    return wait
